e7cf75404ca0e783b342e5f4459f1308a512db20
Azmi117 <kennyhoii665@gmail.com>
2024-12-23 16:03:02 +0700

    Fix: config.js & package.json

--- config/config.js
+++ config/config.js
@@ -5,28 +5,30 @@ if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({ path: require('path').resolve(__dirname, '../.env.test') });
 }
 
-const config = {
-  "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.HOST,
-    "dialect": process.env.DB_DIALECT
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.HOST,
+    dialect: process.env.DB_DIALECT
   },
-  "test": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.HOST,
-    "dialect": process.env.DB_DIALECT
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.HOST,
+    dialect: process.env.DB_DIALECT
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    use_env_variable: 'DATABASE_PUBLIC_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
-};
 
-module.exports = config;
+};
